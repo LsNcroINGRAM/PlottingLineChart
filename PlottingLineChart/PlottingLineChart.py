bc617322f884d2021048a25d5725bc9a71293f9b
Ingram <ingram0192@gmail.com>
2023-04-01 01:00:12 +0800

    加入專案檔案。

--- PlottingLineChart/PlottingLineChart.py
+++ PlottingLineChart/PlottingLineChart.py
@@ -1,7 +1,7 @@
-import random
 from itertools import count
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
+from pandas.errors import EmptyDataError
 from matplotlib.animation import FuncAnimation
 
 plt.style.use('fivethirtyeight')
@@ -11,22 +11,22 @@ y_vals = []
 
 index = count()
 
-
 def animate(i):
-    #data = pd.read_csv(r'\Users\User\source\repos\LsNcroINGRAM\ReadForceSensor\Project1\data.csv')
-    data = pd.read_csv(r'\Users\User\source\repos\LsNcroINGRAM\GeneratingData\GeneratingData\data.csv')
-    x = data['x_value']
-    y = data['y_value']
+    try:
+        data = pd.read_csv(r'\Users\User\source\repos\LsNcroINGRAM\GeneratingData\GeneratingData\data.csv')
+        x = data['x_value']
+        y = data['y_value']
 
-    plt.cla()
+        plt.cla()
 
-    plt.plot(x, y, label = 'Force')
+        plt.plot(x, y, label = 'Force')
 
-    plt.legend(loc = 'upper left')
-    plt.tight_layout()
+        plt.legend(loc = 'upper left')
+        plt.tight_layout()
+    except EmptyDataError:
+        df = pd.DataFrame()
 
-
-ani = FuncAnimation(plt.gcf(), animate, interval = 100)
+ani = FuncAnimation(plt.gcf(), animate, interval = 10)
 
 plt.tight_layout()
 plt.show()
