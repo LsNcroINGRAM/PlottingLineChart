2eb1d84685410f202e09b3ab6a7b0b6e905020bb
Ingram <ingram0192@gmail.com>
2023-04-01 01:00:12 +0800

    加入專案檔案。

--- PlottingLineChart/PlottingLineChart.py
+++ PlottingLineChart/PlottingLineChart.py
@@ -13,19 +13,16 @@ index = count()
 
 def animate(i):
     try:
-        #data = pd.read_csv(r'\Users\User\source\repos\LsNcroINGRAM\GeneratingData\GeneratingData\data.csv')
-        data = pd.read_csv(r'\Users\User\source\repos\LsNcroINGRAM\ReadForceSensor\Project1\data.csv')
+        data = pd.read_csv(r'\Users\User\source\repos\LsNcroINGRAM\GeneratingData\GeneratingData\data.csv')
+        #data = pd.read_csv(r'\Users\User\source\repos\LsNcroINGRAM\ReadForceSensor\Project1\data.csv')
         x = data['x_value']
         y = data['y_value']
-
         plt.cla()
-
         plt.plot(x, y, label = 'Force')
-
         plt.legend(loc = 'upper left')
-        plt.tight_layout()
+
     except EmptyDataError:
-        df = pd.DataFrame()
+        pass
 
 ani = FuncAnimation(plt.gcf(), animate, interval = 5)
 
