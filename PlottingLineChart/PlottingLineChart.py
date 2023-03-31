0e8a2cf9a31083f0ac7aec51a2bbaef94c585dd6
Ingram <ingram0192@gmail.com>
2023-04-01 01:00:12 +0800

    加入專案檔案。

--- PlottingLineChart/PlottingLineChart.py
+++ PlottingLineChart/PlottingLineChart.py
@@ -5,12 +5,13 @@ from pandas.errors import EmptyDataError
 from matplotlib.animation import FuncAnimation
 
 plt.style.use('fivethirtyeight')
+MAX_FRAMES = 100
 
 index = count()
 
 def animate(i):
     try:
-        data = pd.read_csv(r'\Users\User\source\repos\LsNcroINGRAM\GeneratingData\GeneratingData\data.csv') #random value between 0 and 50
+        data = pd.read_csv(r'\Users\User\source\repos\LsNcroINGRAM\ReadForceSensor\Project1\data.csv') #random value between 0 and 100
         #data = pd.read_csv(r'\Users\User\source\repos\LsNcroINGRAM\ReadForceSensor\Project1\data.csv') #from force sensor
         x = data['x_value']
         y = data['y_value']
@@ -22,7 +23,8 @@ def animate(i):
     except EmptyDataError:
         pass
 
-ani = FuncAnimation(plt.gcf(), animate, interval = 10)
+ani = FuncAnimation(plt.gcf(), animate, interval=10, save_count=MAX_FRAMES)
+
 
 plt.tight_layout()
 plt.show()
