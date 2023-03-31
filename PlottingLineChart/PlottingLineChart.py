b9244e58e80309a1724ac6c3f84bf2d8528d243c
Ingram <ingram0192@gmail.com>
2023-04-01 01:00:12 +0800

    加入專案檔案。

--- PlottingLineChart/PlottingLineChart.py
+++ PlottingLineChart/PlottingLineChart.py
@@ -13,11 +13,11 @@ index = count()
 
 def animate(i):
     try:
-        data = pd.read_csv(r'\Users\User\source\repos\LsNcroINGRAM\GeneratingData\GeneratingData\data.csv')
-        #data = pd.read_csv(r'\Users\User\source\repos\LsNcroINGRAM\ReadForceSensor\Project1\data.csv')
+        #data = pd.read_csv(r'\Users\User\source\repos\LsNcroINGRAM\GeneratingData\GeneratingData\data.csv') #random value between 0 and 50
+        data = pd.read_csv(r'\Users\User\source\repos\LsNcroINGRAM\ReadForceSensor\Project1\data.csv') #from force sensor
         x = data['x_value']
         y = data['y_value']
-        plt.cla()
+        plt.cla() #make sure the color of the plot is the same
         plt.plot(x, y, label = 'Force')
         plt.legend(loc = 'upper left')
         plt.ylim(-10, 50)
